2542bd43a522345149f0baf0a9087d600f8c9879
Vishal Shinde <vishalrs@gmail.com>
2020-09-29 21:24:08 +0200

    v2

---
+++ weather-webapp/src/main/java/com/sogeti/weatherapp/WeatherWebappApplication.java
@@ -0,0 +1,13 @@
+package com.sogeti.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWebappApplication.class, args);
+	}
+
+}
---
+++ weather-webapp/src/test/java/com/sogeti/weatherapp/WeatherWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogeti.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
